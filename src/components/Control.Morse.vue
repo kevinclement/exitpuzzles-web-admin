9f803a9b2ee8eeebebddb3701dfee83744d77be3
Kevin Clement <kevinclement@Kevins-MBP.guest.corp.microsoft.com>
2018-03-23 17:33:40 -0700

    morse code card skeleton

--- src/components/Control.Morse.vue
+++ src/components/Control.Morse.vue
@@ -1,13 +1,24 @@
 <template>
  
-<v-flex>
-       <!-- sample card two -->
-      <v-card id="cardtwo">
-          a card
-      </v-card>
+<v-flex>  
+  <!-- morse code -->
+  <v-card class="morseCard">
+    <v-toolbar card>
+      <v-toolbar-title>Morse Code</v-toolbar-title>
+      <span class="spacer" />
+    </v-toolbar>
+
+    <v-card-text class="grey lighten-3">
+      body
+    </v-card-text>
+
+  </v-card>
 </v-flex>
 
 </template>
 
 <style scoped>
+.morseCard {
+  margin-top: 30px;
+}
 </style>
