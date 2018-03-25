ffb87aef9063d0cdb3f5ac3b5defe20ddf259750
Kevin Clement <kevin.clement@gmail.com>
2018-03-24 17:19:51 -0700

    add message dialog

--- src/components/Control.Morse.vue
+++ src/components/Control.Morse.vue
@@ -1,20 +1,45 @@
 <template>
  
-<v-flex>  
+<v-flex>
+  <!-- dialogs -->
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">{{ formTitle }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap>
+            <v-flex xs12>
+              <v-text-field label="Line 1" v-model="editedItem.line1"></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field label="Line 2" v-model="editedItem.line2"></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+        <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
   <!-- morse code -->
   <v-card class="morseCard">
     <v-toolbar card>
       <v-toolbar-title>Morse Code</v-toolbar-title>
       <span class="spacer" />
 
-      <v-btn icon title="Add a message" @click.native=""><v-icon >message</v-icon></v-btn>
+      <v-btn icon title="Send a message" @click.native="adhocSend"><v-icon >message</v-icon></v-btn>
       <v-btn icon title="Edit messages" @click.native="editMode = !editMode"><v-icon >edit</v-icon></v-btn>
-      <v-btn icon title="Add a message" @click.native=""><v-icon >add</v-icon></v-btn>
+      <v-btn icon title="Add a message" @click.native="dialog=true"><v-icon>add</v-icon></v-btn>
     </v-toolbar>
 
     <v-card-text class="grey lighten-3">
       <v-data-table
-      :headers="headers"
       :items="items"
       hide-actions
       hide-headers
@@ -24,7 +49,7 @@
           <td>{{ props.item.line1 }} {{ props.item.line2 }}</td>
           <td class="text-xs-right ">
             <v-btn v-if="editMode" icon class="mx-0" @click="deleteItem(props.item)">
-              <v-icon  color="pink">delete</v-icon>
+              <v-icon  color="red lighten-1">delete</v-icon>
             </v-btn>
             <v-btn v-if="!editMode" icon class="mx-0" @click="sendClue(props.item)">
               <v-icon  color="blue accent-1">announcement</v-icon>
@@ -47,26 +72,20 @@
     data: () => ({
       dialog: false,
       editMode: false,
-      headers: [
-        {
-          text: 'Message',
-          align: 'left',
-          sortable: false
-        },
-        { align: 'right', text: '', sortable: false }
-      ],
       items: [],
       editedIndex: -1,
       editedItem: {
-        message: ''
+        line1: '',
+        line2: ''
       },
       defaultItem: {
-        message: ''
+        line1: '',
+        line2: ''
       }
     }),
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? 'New Clue' : 'Edit Clue'
       }
     },
     watch: {
@@ -94,6 +113,9 @@
       },
       sendClue (item) {
         console.log('sending clue' + item.line1 + ' ' + item.line2);
+      },
+      adhocSend() {
+        console.log('adhoc');
       },
       deleteItem (item) {
         const index = this.items.indexOf(item)
