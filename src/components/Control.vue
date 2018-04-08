6403d9f25204cee3c1a1bb4df7fe94281e155011
Kevin Clement <kevin.clement@gmail.com>
2018-04-08 14:17:59 -0700

    report pi status

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -1,5 +1,13 @@
 <template>
   <v-container fluid>
+      <v-alert
+        color="error"
+        :value="alert"
+        icon="new_releases"
+        transition="scale-transition"
+      >
+        Raspberry Pi is offline.  Last ping at {{piPingFormatted}}. See <router-link style="color:white" to="/logs">logs</router-link> for more info.
+      </v-alert>
       <control-tnt :snack="showSnack"></control-tnt>
       <control-morse :snack="showSnack"></control-morse>
       <v-snackbar :timeout="snackTimeout" :color="snackColor" v-model="snackbar">
@@ -14,6 +22,9 @@
 import TNT from '@/components/Control.TNT'
 import Morse from '@/components/Control.Morse'
 
+// how much time before we know pi isn't connected (in seconds)
+const PI_TIMEOUT_SECONDS = 65
+
 export default {
   data () {
     return {
@@ -23,8 +34,41 @@ export default {
       snackColor: 'success',
       snackTimeout: 4000,
       snackText: '',
-
+      alert:false,
+      piPing:0
     }
+  },
+
+  computed: {
+    piPingFormatted: function() {
+      return (new Date(this.piPing)).toLocaleString()
+    }
+  },
+
+  mounted() {
+
+    this.statusRef = this.$root.$data.fbdb.ref('status')
+    this.statusRef.on('value', (snapshot) => {
+      let status = snapshot.val()
+      if (status == null) return
+
+      if (status.piPing) {
+        this.piPing = status.piPing
+      }
+    })
+
+    setInterval(() => {
+      // check to make sure pi is still connected
+      // pi will write ping time 
+      let now = (new Date()).getTime()
+      let elapsedSeconds = Math.floor((now - this.piPing) / 1000)
+
+      if (elapsedSeconds > PI_TIMEOUT_SECONDS) {
+        this.alert = true
+      } else {
+        this.alert = false
+      }
+    }, 1000)
   },
 
   methods: {
@@ -34,7 +78,6 @@ export default {
     }
   },
 
-
   components: {
     'control-tnt': TNT,
     'control-morse': Morse
@@ -43,5 +86,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
