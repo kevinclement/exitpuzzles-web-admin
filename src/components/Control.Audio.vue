a1230c3769f8e6a8046af754ce7d9e2f8ac305fe
Kevin Clement <kevin.clement@gmail.com>
2018-05-20 23:55:07 -0700

    put in buttons

--- src/components/Control.Audio.vue
+++ src/components/Control.Audio.vue
@@ -12,9 +12,27 @@
 
       <v-card-text class="grey lighten-3">
         <v-layout row wrap>
-          <v-flex xs12 md6>
-            PUT AUDIO BUTTONS HERE
+
+          <v-flex xs12 sm6>
+
+            <v-btn color="blue accent-1" fab dark>
+              <v-icon>filter_1</v-icon>
+            </v-btn>
+            <v-btn color="blue accent-1" fab dark>
+              <v-icon>filter_2</v-icon>
+            </v-btn>
+            <v-btn color="blue accent-1" fab dark>
+              <v-icon>filter_3</v-icon>
+            </v-btn>
+            <v-btn color="blue accent-1" fab dark>
+              <v-icon>filter_4</v-icon>
+            </v-btn>
+            <v-btn color="blue accent-1" fab dark>
+              <v-icon>filter_5</v-icon>
+            </v-btn>
+
           </v-flex>
+
         </v-layout>
       </v-card-text>
     </v-card>
