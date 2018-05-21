ca29a9074d6e16848b9aa2edbcb0aae7dd5c3964
Kevin Clement <kevin.clement@gmail.com>
2018-05-21 00:41:24 -0700

    cleanup stale comments

--- src/components/Control.Audio.vue
+++ src/components/Control.Audio.vue
@@ -63,26 +63,14 @@ export default {
     this.operations = this.$root.$data.operations
     this.audioRef = this.$root.$data.fbdb.ref('audio')
 
+    // TMP: used to build out files in DB
+    //this.audioRef.push({ name: 'File 1', icon: 'filter_1', file: 'TBD2.wav'});
+
     this.audioRef.on("child_added", (snapshot) => {
       var audio = snapshot.val();
       this.files.push(audio)
     });
-    //this.audioRef.push({ name: 'File 1', icon: 'filter_1', file: 'TBD2.wav'});
 
-    // this.audioRef.child('time').on('value', (snapshot) => {
-    //   let time = snapshot.val()
-    //   if (time == null) return
-
-    //   // update might be partial, so fill out from our state
-    //   let h = time.hours ? time.hours : this.hours
-    //   let m = time.minutes ? time.minutes : this.minutes
-    //   let s = time.seconds ? time.seconds : this.seconds
-    //   let ts = time.timestamp ? time.timestamp : this.timerTimeStamp
-    //   let elapsed = Math.floor(((new Date()).getTime() - ts) / 1000)
-
-    //   this.timeLeftInSeconds = (h * 3600) + (m * 60) + s - elapsed
-    //   this.timerTimeStamp = ts
-    // });
   }
 
 }
