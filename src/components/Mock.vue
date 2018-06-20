af2d52512382503e2bf502de84fa146b9f36fb01
Kevin Clement <kevin.clement@gmail.com>
2018-06-19 17:36:06 -0700

    added last password to mock page

--- src/components/Mock.vue
+++ src/components/Mock.vue
@@ -1,12 +1,40 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        MOCK
+      <v-layout>
+        <v-flex xs2>
+          <v-text-field v-model="lastPassword" label="Last Password"></v-text-field>
+          <v-btn color="info" @click="save">Save</v-btn>
+        </v-flex>
       </v-layout>
+
     </v-slide-y-transition>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      lastPassword: ''
+    }),
+
+    mounted() {
+      this.morseRef = this.$root.$data.fbdb.ref('morse')
+      this.tntRef = this.$root.$data.fbdb.ref('tnt')
+
+    },
+
+    methods: {
+      save() {
+        this.tntRef.child('state').set(
+          {
+            lastBadPassword: this.lastPassword
+          }
+        )
+      }
+    }
+  }
+</script>
 
 <style scoped>
 </style>
