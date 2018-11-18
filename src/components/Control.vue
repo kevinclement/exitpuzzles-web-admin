b8936769b79319f0fea546d4916732c499ab747a
Kevin Clement <kevin.clement@gmail.com>
2018-11-18 01:12:37 -0800

    move painting down

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -9,11 +9,10 @@
         Raspberry Pi is offline.  Last ping at {{piPingFormatted}}. See <router-link style="color:white" to="/logs">logs</router-link> for more info.
       </v-alert>
       <control-tnt :snack="showSnack"></control-tnt>
-      <control-painting :snack="showSnack"></control-painting>
-      <!-- TODO: move back down when done -->
       <control-audio :snack="showSnack"></control-audio>
       <control-morse :snack="showSnack"></control-morse>
-      
+      <control-painting :snack="showSnack"></control-painting>
+
       <v-snackbar :timeout="snackTimeout" :color="snackColor" v-model="snackbar">
         {{ snackText }}
       <v-btn dark flat @click.native="snackbar = false">Close</v-btn>
