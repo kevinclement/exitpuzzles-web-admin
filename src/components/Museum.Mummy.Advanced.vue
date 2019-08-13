f433e56e0c413133e5af083f90299cb1d255ef4d
Kevin Clement <kevin.clement@gmail.com>
2019-08-12 21:58:07 -0700

    get values from db

--- src/components/Museum.Mummy.Advanced.vue
+++ src/components/Museum.Mummy.Advanced.vue
@@ -8,12 +8,31 @@
 
 <script>
   export default {
-    props: ['snack'],
+    props: [],
+
+    // original number before I stored them in DB, DB should be source of truth
     data: () => ({
+      debounce: 500,
+      lightning_number_flashes_max: 7,
+      lightning_time_between_events: 2579,
+      lightning_time_between_flashes: 22,
+      measure_one_sensitivity: 1200,
+      measure_two_sensitivity: 1000,
     }),
     computed: {
     },
     created () {
+      this.$root.$data.museumRoot.child('mummy').on('value', (snapshot) => {
+        let mummy = snapshot.val()
+        if (mummy == null) return
+
+        this.debounce = mummy.debounce
+        this.lightning_number_flashes_max = mummy.lightning_number_flashes_max
+        this.lightning_time_between_events = mummy.lightning_time_between_events
+        this.lightning_time_between_flashes = mummy.lightning_time_between_flashes
+        this.measure_one_sensitivity = mummy.measure_one_sensitivity
+        this.measure_two_sensitivity = mummy.measure_two_sensitivity
+      })
     },
     methods: {
       trigger() {
