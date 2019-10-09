279f63ee64c19f264054f01318d1f945623ca696
Kevin Clement <kevin.clement@gmail.com>
2019-10-09 16:09:34 -0700

    add reboot to quiz

--- src/components/Museum.Quiz.vue
+++ src/components/Museum.Quiz.vue
@@ -17,6 +17,8 @@
       {{currentQuestion}}/{{total}}
       missed: {{missed.length}}
       </span>
+
+      <v-btn flat small color="red lighten-3" @click.native="dialogReset = true">Reboot</v-btn>
     </v-toolbar>
   </v-card>
 
@@ -32,6 +34,17 @@
     </v-card>
   </v-dialog>
   
+  <v-dialog v-model="dialogReset" max-width="410">
+    <v-card>
+      <v-card-title class="headline">Really reset the quiz?</v-card-title>
+      <v-card-text>This will clear the state and reset it to title screen.  Are you <i>sure</i> you want to do that?</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" flat="flat" @click.native="dialogReset = false;">No</v-btn>
+        <v-btn color="primary" flat="flat" @click.native="triggerReset">Yes</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </v-flex>
 </template>
 
@@ -48,6 +61,7 @@
       missed: [],
       correct: [],
       total: 0,
+      dialogReset: false,
     }),
     created () {
       // INSERT_DISK, LAUNCH, QUIZ, PASSWORD, JOURNAL
@@ -77,6 +91,15 @@
           force: this.dialogForce
         });
 
+      },
+
+      triggerReset() {
+        this.dialogReset = false
+
+        this.$root.$data.museumRoot.child('devices/quiz').update({
+          force: 4
+        });
+
       }
     }
   }
@@ -98,6 +121,5 @@
 .results {
   font-family: Monaco, monospace;
   font-size:16px;
-  padding-right: 0px; /* 88px */
 }
 </style>
