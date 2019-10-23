9ebf4b680793e1b8d11a2f0e4459807165463109
Kevin Clement <kevin.clement@gmail.com>
2019-10-23 00:59:31 -0700

    set timer on reset

--- src/components/Museum.ResetDialog.vue
+++ src/components/Museum.ResetDialog.vue
@@ -9,42 +9,18 @@
       <v-divider></v-divider>
 
       <v-list class="resetRoom" dense>
-        <v-list-tile>
-          <v-list-tile-content>Clock</v-list-tile-content>
+        <v-list-tile v-for="d in devices" :key="d.dev">
+          <v-list-tile-content>{{d.name}}</v-list-tile-content>
           <v-list-tile-content class="align-end">
-            <v-icon style="color:#4CAF50">done</v-icon>
+            <v-icon :style="{ color: iconColor(d) }">check</v-icon>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>Quiz</v-list-tile-content>
-          <v-list-tile-content class="align-end">
-            <v-icon style="color:#BDBDBD">check</v-icon>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>Birdcage</v-list-tile-content>
-          <v-list-tile-content class="align-end">
-            <v-icon style="color:#BDBDBD">check</v-icon>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>Zoltar</v-list-tile-content>
-          <v-list-tile-content class="align-end">
-            <v-icon>check</v-icon>
-          </v-list-tile-content>
-        </v-list-tile>
-        
-        <v-list-tile>
-          <v-list-tile-content>Cabinet</v-list-tile-content>
-          <v-list-tile-content class="align-end">
-            <v-icon style="">check</v-icon>
-          </v-list-tile-content>
-        </v-list-tile>
+
         <v-divider/>
 
         <v-list-tile style="padding-top:5px;">
           <v-list-tile-content class="align-end">
-            Time Taken: 00:33
+            Time Taken: {{runningTimePretty}}
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -58,20 +34,46 @@
     props: ['operations'],
 
     data: () => ({
-      show: true
+      show: true,
+      runningTime: 0,
+      devices: [
+        { name: 'Clock',    reset: false, dev:'clock' },
+        { name: 'Quiz',     reset: false, dev:'quiz' },
+        { name: 'Birdcage', reset: false, dev:'bird' },
+        { name: 'Zoltar',   reset: false, dev:'zoltar' },
+        { name: 'Mummy',    reset: false, dev:'mummy' },
+        { name: 'Stairs',   reset: false, dev:'stairs' },
+        { name: 'Cabinet',  reset: false, dev:'cabinet' }
+      ]
     }),
     computed: {
+      runningTimePretty: function() {
+        let m = Math.floor(this.runningTime / 60)
+        let s = this.runningTime % 60
+
+        m = m < 10 ? "0" + m : m
+        s = s < 10 ? "0" + s : s
+
+        return `${m}:${s}`
+      }
     },
-    created () {
+    mounted() {
+      setInterval(() => {
+        this.runningTime++;
+      }, 1000)
     },
     methods: {
+      iconColor: function(d) {
+        if (d.reset) {
+          return '#4CAF50'
+        } else {
+          return '#BDBDBD'
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
-  .resetRoom i {
-    color:#BDBDBD !important;
-  }
 </style>
 
