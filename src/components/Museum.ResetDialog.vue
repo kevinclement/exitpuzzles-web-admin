f51d7aac3fb79c5a0556c8395fc2f82ad2c59a32
Kevin Clement <kevin.clement@gmail.com>
2019-10-23 02:43:38 -0700

    bottom bar

--- src/components/Museum.ResetDialog.vue
+++ src/components/Museum.ResetDialog.vue
@@ -19,10 +19,13 @@
 
         <v-divider/>
 
-        <v-list-tile style="padding-top:5px;">
-          <v-list-tile-content class="align-end">
+        <v-list-tile style="padding-top:10px;padding-bottom:5px;">
+          <v-list-tile-content class="">
             Time Taken: {{runningTimePretty}}
           </v-list-tile-content>
+          <v-list-tile-content class="align-end">
+          <v-btn :disabled="true" color="primary"  @click.native="">Running</v-btn>
+          </v-list-tile-content class="">
         </v-list-tile>
       </v-list>
     </v-card>
