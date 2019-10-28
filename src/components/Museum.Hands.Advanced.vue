fb5759ce820bde3ce6ec348bfd64886b56bc401f
Kevin Clement <kevin.clement@gmail.com>
2019-10-28 00:35:54 -0700

    map colors proper

--- src/components/Museum.Hands.Advanced.vue
+++ src/components/Museum.Hands.Advanced.vue
@@ -12,43 +12,35 @@
   </div>
   <div class="row" style="padding-top:15px;">
     <table border=0>
-      <tr><td>1-1:</td><td class="ans"><span :class="getClass('1-1')">{{state['1-1']}}</span></td></tr>
-      <tr><td>1-2:</td><td class="ans"><span :class="getClass('1-2')">{{state['1-2']}}</span></td></tr>
-      <tr><td>1-3:</td><td class="ans"><span :class="getClass('1-3')">{{state['1-3']}}</span></td></tr>
+      <tr><td>1-1:</td><td class="ans"><span :class="getClass('1-1')">{{getColor(state['1-1'])}}</span></td></tr>
+      <tr><td>1-2:</td><td class="ans"><span :class="getClass('1-2')">{{getColor(state['1-2'])}}</span></td></tr>
+      <tr><td>1-3:</td><td class="ans"><span :class="getClass('1-3')">{{getColor(state['1-3'])}}</span></td></tr>
 
       <tr><td class="rowPadding"></td></tr>
-      <tr><td>2-1:</td><td class="ans"><span :class="getClass('2-1')">{{state['2-1']}}</span></td></tr>
-      <tr><td>2-2:</td><td class="ans"><span :class="getClass('2-2')">{{state['2-2']}}</span></td></tr>
-      <tr><td>2-3:</td><td class="ans"><span :class="getClass('2-3')">{{state['2-3']}}</span></td></tr>
+      <tr><td>2-1:</td><td class="ans"><span :class="getClass('2-1')">{{getColor(state['2-1'])}}</span></td></tr>
+      <tr><td>2-2:</td><td class="ans"><span :class="getClass('2-2')">{{getColor(state['2-2'])}}</span></td></tr>
+      <tr><td>2-3:</td><td class="ans"><span :class="getClass('2-3')">{{getColor(state['2-3'])}}</span></td></tr>
 
       <tr><td class="rowPadding"></td></tr>
-      <tr><td>3-1:</td><td class="ans"><span :class="getClass('3-1')">{{state['3-1']}}</span></td></tr>
-      <tr><td>3-2:</td><td class="ans"><span :class="getClass('3-2')">{{state['3-2']}}</span></td></tr>
-      <tr><td>3-3:</td><td class="ans"><span :class="getClass('3-3')">{{state['3-3']}}</span></td></tr>
+      <tr><td>3-1:</td><td class="ans"><span :class="getClass('3-1')">{{getColor(state['3-1'])}}</span></td></tr>
+      <tr><td>3-2:</td><td class="ans"><span :class="getClass('3-2')">{{getColor(state['3-2'])}}</span></td></tr>
+      <tr><td>3-3:</td><td class="ans"><span :class="getClass('3-3')">{{getColor(state['3-3'])}}</span></td></tr>
 
       <tr><td class="rowPadding"></td></tr>
-      <tr><td>4-1:</td><td class="ans"><span :class="getClass('4-1')">{{state['4-1']}}</span></td></tr>
-      <tr><td>4-2:</td><td class="ans"><span :class="getClass('4-2')">{{state['4-2']}}</span></td></tr>
-      <tr><td>4-3:</td><td class="ans"><span :class="getClass('4-3')">{{state['4-3']}}</span></td></tr>
+      <tr><td>4-1:</td><td class="ans"><span :class="getClass('4-1')">{{getColor(state['4-1'])}}</span></td></tr>
+      <tr><td>4-2:</td><td class="ans"><span :class="getClass('4-2')">{{getColor(state['4-2'])}}</span></td></tr>
+      <tr><td>4-3:</td><td class="ans"><span :class="getClass('4-3')">{{getColor(state['4-3'])}}</span></td></tr>
 
       <tr><td class="rowPadding"></td></tr>
-      <tr><td>5-1:</td><td class="ans"><span :class="getClass('5-1')">{{state['5-1']}}</span></td></tr>
-      <tr><td>5-2:</td><td class="ans"><span :class="getClass('5-2')">{{state['5-2']}}</span></td></tr>
-      <tr><td>5-3:</td><td class="ans"><span :class="getClass('5-3')">{{state['5-3']}}</span></td></tr>
+      <tr><td>5-1:</td><td class="ans"><span :class="getClass('5-1')">{{getColor(state['5-1'])}}</span></td></tr>
+      <tr><td>5-2:</td><td class="ans"><span :class="getClass('5-2')">{{getColor(state['5-2'])}}</span></td></tr>
+      <tr><td>5-3:</td><td class="ans"><span :class="getClass('5-3')">{{getColor(state['5-3'])}}</span></td></tr>
 
       <tr><td class="rowPadding"></td></tr>
-      <tr><td>6-1:</td><td class="ans"><span :class="getClass('6-1')">{{state['6-1']}}</span></td></tr>
-      <tr><td>6-2:</td><td class="ans"><span :class="getClass('6-2')">{{state['6-2']}}</span></td></tr>
-      <tr><td>6-3:</td><td class="ans"><span :class="getClass('6-3')">{{state['6-3']}}</span></td></tr>
-    </table>
-    <div style="padding-top: 20px;">
-      {{stats.start}}
-    </div>
-    <div >
-      connected: {{percentConnected}}%
-    </div>
-    
-    
+      <tr><td>6-1:</td><td class="ans"><span :class="getClass('6-1')">{{getColor(state['6-1'])}}</span></td></tr>
+      <tr><td>6-2:</td><td class="ans"><span :class="getClass('6-2')">{{getColor(state['6-2'])}}</span></td></tr>
+      <tr><td>6-3:</td><td class="ans"><span :class="getClass('6-3')">{{getColor(state['6-3'])}}</span></td></tr>
+    </table>    
   </div>
 </div>
 </template>
@@ -82,46 +74,14 @@
         "6-1": "",
         "6-2": "",
         "6-3": ""
-      },
-      stats: {
-        connects: 0,
-        disconnects: 0,
-        start: "",
-        timeDisconnectedMS: 0
       }
     }),
     computed: {
-      percentConnected: function() {
-      
-        if (this.stats.start == "") {
-          return 0
-        }
-
-        var then = new Date(this.stats.start)
-        var now = new Date()
-        var totalMS = (now - then) * 18
-        var totalConnected = totalMS - this.stats.timeDisconnectedMS
-        var perc = (totalConnected / totalMS) * 100
-
-        // just grab 2 decimal places
-        var parts = perc.toString().split('.')
-        var p1 = parts[0]
-        var p2 = parts[1].substring(0,2)
-
-        return `${p1}.${p2}`;
-      }
     },
     created () {
       this.$root.$data.museumRoot.child('devices/bulbs').on('value', (snapshot) => {
         let bulb = snapshot.val()
         if (bulb == null) return
-
-        if (bulb.stats) {
-          this.stats.connects = bulb.stats.connects;
-          this.stats.disconnects = bulb.stats.disconnects;
-          this.stats.start = bulb.stats.start;
-          this.stats.timeDisconnectedMS = bulb.stats.timeDisconnectedMS;
-        }
 
         for (const [b, state] of Object.entries(bulb)) {
           this.state[b] = state
@@ -132,15 +92,24 @@
       getClass(bulb) {
         let c = ''
 
-        if (this.state[bulb] === 'connected') {
-          c = 'yes'
-        } else if (this.state[bulb] === 'connecting') {
-          c = 'maybe'
+        let sc = this.state[bulb].color
+        if (sc === 'r') {
+          c = 'r'
+        } else if (this.state[bulb].color === 'g') {
+          c = 'g'
+        } else if (this.state[bulb].color === 'b') {
+          c = 'b' 
         } else {
-          c = 'no'
+          c = 'w'
         }
 
         return c
+      },
+      getColor(bulb) {
+        return bulb.color == "r" ? "red" : 
+          bulb.color == "g" ? "green" : 
+          bulb.color == "b" ? "blue" : 
+           "white"
       }
     }
   }
@@ -153,7 +122,7 @@
     font-family: Monaco, monospace;
     font-size:16px;
   }
-  .no, .yes, .maybe {
+  .g, .r, .b, .w {
     display: inline-block;
     width: 125px;
     height: 22px;
@@ -161,11 +130,17 @@
     color: white;
     text-align: center;
   }
-  .yes {
+  .w {
+    background:lightgray;
+  }
+  .g {
     background: green;
   }
-  .no, .maybe{
+  .r {
     background: red;
+  }
+  .b {
+    background: blue;
   }
   .ans {
     padding-left:35px;
