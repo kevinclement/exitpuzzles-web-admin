3fe489653646a20105b7b2c15bd8000a44bfe600
Kevin Clement <kevin.clement@gmail.com>
2019-10-28 01:07:20 -0700

    use for loops

--- src/components/Museum.Hands.Advanced.vue
+++ src/components/Museum.Hands.Advanced.vue
@@ -12,34 +12,10 @@
   </div>
   <div class="row" style="padding-top:15px;">
     <table border=0>
-      <tr><td>1-1:</td><td class="ans"><span :class="getClass('1-1')">{{getColor(state['1-1'])}}</span></td></tr>
-      <tr><td>1-2:</td><td class="ans"><span :class="getClass('1-2')">{{getColor(state['1-2'])}}</span></td></tr>
-      <tr><td>1-3:</td><td class="ans"><span :class="getClass('1-3')">{{getColor(state['1-3'])}}</span></td></tr>
-
-      <tr><td class="rowPadding"></td></tr>
-      <tr><td>2-1:</td><td class="ans"><span :class="getClass('2-1')">{{getColor(state['2-1'])}}</span></td></tr>
-      <tr><td>2-2:</td><td class="ans"><span :class="getClass('2-2')">{{getColor(state['2-2'])}}</span></td></tr>
-      <tr><td>2-3:</td><td class="ans"><span :class="getClass('2-3')">{{getColor(state['2-3'])}}</span></td></tr>
-
-      <tr><td class="rowPadding"></td></tr>
-      <tr><td>3-1:</td><td class="ans"><span :class="getClass('3-1')">{{getColor(state['3-1'])}}</span></td></tr>
-      <tr><td>3-2:</td><td class="ans"><span :class="getClass('3-2')">{{getColor(state['3-2'])}}</span></td></tr>
-      <tr><td>3-3:</td><td class="ans"><span :class="getClass('3-3')">{{getColor(state['3-3'])}}</span></td></tr>
-
-      <tr><td class="rowPadding"></td></tr>
-      <tr><td>4-1:</td><td class="ans"><span :class="getClass('4-1')">{{getColor(state['4-1'])}}</span></td></tr>
-      <tr><td>4-2:</td><td class="ans"><span :class="getClass('4-2')">{{getColor(state['4-2'])}}</span></td></tr>
-      <tr><td>4-3:</td><td class="ans"><span :class="getClass('4-3')">{{getColor(state['4-3'])}}</span></td></tr>
-
-      <tr><td class="rowPadding"></td></tr>
-      <tr><td>5-1:</td><td class="ans"><span :class="getClass('5-1')">{{getColor(state['5-1'])}}</span></td></tr>
-      <tr><td>5-2:</td><td class="ans"><span :class="getClass('5-2')">{{getColor(state['5-2'])}}</span></td></tr>
-      <tr><td>5-3:</td><td class="ans"><span :class="getClass('5-3')">{{getColor(state['5-3'])}}</span></td></tr>
-
-      <tr><td class="rowPadding"></td></tr>
-      <tr><td>6-1:</td><td class="ans"><span :class="getClass('6-1')">{{getColor(state['6-1'])}}</span></td></tr>
-      <tr><td>6-2:</td><td class="ans"><span :class="getClass('6-2')">{{getColor(state['6-2'])}}</span></td></tr>
-      <tr><td>6-3:</td><td class="ans"><span :class="getClass('6-3')">{{getColor(state['6-3'])}}</span></td></tr>
+      <tr v-for="(bulb, index) in state" :key="bulb.name" :class="{ extraPadding:(index + 1) % 3 == 0 }">
+        <td>{{bulb.name}}</td>
+        <td class="ans"><span :class="getClass(bulb.state.color)">{{getColor(bulb.state.color)}}</span></td>
+      </tr>
     </table>    
   </div>
 </div>
@@ -50,31 +26,7 @@
     props: ['operations'],
 
     data: () => ({
-      state: {
-        "1-1": "",
-        "1-2": "",
-        "1-3": "",
-
-        "2-1": "",
-        "2-2": "",
-        "2-3": "",
-
-        "3-1": "",
-        "3-2": "",
-        "3-3": "",
-
-        "4-1": "",
-        "4-2": "",
-        "4-3": "",
-
-        "5-1": "",
-        "5-2": "",
-        "5-3": "",
-
-        "6-1": "",
-        "6-2": "",
-        "6-3": ""
-      }
+      state: [],
     }),
     computed: {
     },
@@ -83,32 +35,20 @@
         let bulb = snapshot.val()
         if (bulb == null) return
 
+        this.state = [];
         for (const [b, state] of Object.entries(bulb)) {
-          this.state[b] = state
+          this.state.push({ name: b, state: state })
         }
       })
     },
     methods: {
-      getClass(bulb) {
-        let c = ''
-
-        let sc = this.state[bulb].color
-        if (sc === 'r') {
-          c = 'r'
-        } else if (this.state[bulb].color === 'g') {
-          c = 'g'
-        } else if (this.state[bulb].color === 'b') {
-          c = 'b' 
-        } else {
-          c = 'w'
-        }
-
-        return c
+      getClass(color) {
+        return color == 'white' ? 'w' : color
       },
-      getColor(bulb) {
-        return bulb.color == "r" ? "red" : 
-          bulb.color == "g" ? "green" : 
-          bulb.color == "b" ? "blue" : 
+      getColor(color) {
+        return color == "r" ? "red" : 
+          color == "g" ? "green" : 
+          color == "b" ? "blue" : 
            "white"
       }
     }
@@ -145,8 +85,9 @@
   .ans {
     padding-left:35px;
   }
-  .rowPadding {
-    padding-top:15px;
+  .extraPadding {
+    vertical-align: top;
+    height: 35px;
   }
 </style>
 
