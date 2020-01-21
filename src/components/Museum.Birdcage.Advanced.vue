a781393bc61c871ce6851ee333cc3488f0a7045f
Kevin Clement <kevin.clement@gmail.com>
2020-01-20 18:25:16 -0800

    increase padding for out and in

--- src/components/Museum.Birdcage.Advanced.vue
+++ src/components/Museum.Birdcage.Advanced.vue
@@ -10,7 +10,7 @@
   <div class="actionRow">
     <v-btn small color="red lighten-3" @click.native="$root.$emit('reboot-device', 'bird')">Reboot</v-btn>
   </div>
-  <div class="actionRow" style="padding-top:15px;">
+  <div class="actionRow" style="padding-top:100px;">
     <v-btn @click.native="trayBack"><v-icon>navigate_before</v-icon>In</v-btn>
     <v-btn @click.native="trayForward">Out<v-icon>navigate_next</v-icon></v-btn>
   </div>
