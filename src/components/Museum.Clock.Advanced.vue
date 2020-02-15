03cdba8e27bee4d35fe96bfd3e4be24e80540ea9
Kevin Clement <kevin.clement@gmail.com>
2020-02-14 22:21:23 -0800

    clock control

--- src/components/Museum.Clock.Advanced.vue
+++ src/components/Museum.Clock.Advanced.vue
@@ -11,6 +11,21 @@
     <v-btn small color="red lighten-3" @click.native="$root.$emit('reboot-device', 'clock')">Reboot</v-btn>
   </div>
 
+  <div class="actionRow" style="padding-top:100px;padding-left:15px;font-size:18px;font-weight:400;">
+    HOUR
+  </div>
+  <div class="actionRow">
+    <v-btn @click.native="hourInc"><v-icon>add</v-icon></v-btn>
+    <v-btn @click.native="hourDec"><v-icon>remove</v-icon></v-btn>
+  </div>
+
+  <div class="actionRow" style="padding-top:20px;padding-left:15px;font-size:18px;font-weight:400;">
+    MINUTE
+  </div>
+  <div class="actionRow">
+    <v-btn @click.native="minuteInc"><v-icon>add</v-icon></v-btn>
+    <v-btn @click.native="minuteDec"><v-icon>remove</v-icon></v-btn>
+  </div>
 </div>
 </template>
 
@@ -29,6 +44,18 @@
       })
     },
     methods: {
+      hourInc() {
+        this.operations.add({ command: 'clock.hour' }).on("value", (snapshot) => {});
+      },
+      hourDec() {
+        this.operations.add({ command: 'clock.minute' }).on("value", (snapshot) => {});
+      },
+      minuteInc() {
+        this.operations.add({ command: 'clock.hour' }).on("value", (snapshot) => {});
+      },
+      minuteDec() {
+        this.operations.add({ command: 'clock.minDec' }).on("value", (snapshot) => {});
+      },
     }
   }
 </script>
